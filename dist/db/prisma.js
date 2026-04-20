3a564b1d899f899d8d8770ac6769e6f3051a46a0
Artem Maysky <ijezz@List.ru>
2026-04-20 03:41:00 +0300

    fix13

--- dist/db/prisma.js
+++ dist/db/prisma.js
@@ -1,4 +1,5 @@
 import { PrismaClient } from "@prisma/client";
+console.log("DATABASE_URL:", process.env.DATABASE_URL);
 const prisma = new PrismaClient();
 export default prisma;
 //# sourceMappingURL=prisma.js.map
--- prisma.config.ts
+++ prisma.config.ts
@@ -1,6 +1,7 @@
 import { DATABASE_URL } from "./src/config/env.config.js";
 import { defineConfig } from "prisma/config";
 
+
 export default defineConfig({
   datasource: {
       url: DATABASE_URL
