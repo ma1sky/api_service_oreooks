d7307ebae31c01283ff62307640d858a568fcc5d
Artem Maysky <ijezz@List.ru>
2026-04-24 08:27:23 +0300

    fix

--- dist/routes/routes.js
+++ dist/routes/routes.js
@@ -1,9 +1,10 @@
 import { Router } from "express";
 import { auth } from "../controllers/auth.controller.js";
-import { createTask, getTasksByUser } from "../controllers/task.controller.js";
+import { createTask, deleteTask, getTasksByUser } from "../controllers/task.controller.js";
 const router = Router();
 router.post('/auth', auth);
 router.post('/users/:tgId/tasks', createTask);
 router.get('/users/:tgId/tasks', getTasksByUser);
+router.delete('/users/:tgId/tasks/:id', deleteTask);
 export default router;
 //# sourceMappingURL=routes.js.map
