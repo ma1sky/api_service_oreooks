4472360ebc760d2c2b59b4be6e65ffffe0a492c2
Artem Maysky <ijezz@List.ru>
2026-04-30 18:28:58 +0300

    fix

--- src/config/server.config.ts
+++ src/config/server.config.ts
@@ -8,7 +8,7 @@ export default function createApp() {
     server.use(express.json());
     server.use(router);
 
-    server.listen(PORT, '0.0.0.0', () => {
+    server.listen(PORT, () => {
         console.log(`Server is running on port ${PORT}`);
     });
 
