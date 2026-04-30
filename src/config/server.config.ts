a541f23a608665445eeec2aa206db29df7dd9fce
Artem Maysky <ijezz@List.ru>
2026-04-30 18:21:43 +0300

    fix

--- src/config/server.config.ts
+++ src/config/server.config.ts
@@ -1,16 +1,16 @@
 import express from 'express';
-import { PORT } from './env.config'
+import { PORT } from './env.config';
 import router from '../routes/routes';
 
 export default function createApp() {
-    
     const server = express();
-    
+
     server.use(express.json());
     server.use(router);
-    server.listen(PORT, () => {
-          console.log(`Server is running on http://localhost:${PORT}`)
-    })
+
+    server.listen(PORT, '0.0.0.0', () => {
+        console.log(`Server is running on port ${PORT}`);
+    });
 
     return server;
 }
