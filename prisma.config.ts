1440488cc10848b5471a845dc44dc4366561de93
Artem Maysky <ijezz@List.ru>
2026-04-30 12:50:32 +0300

    fix

--- prisma.config.ts
+++ prisma.config.ts
@@ -1,9 +1,10 @@
-import { DATABASE_URL } from "./src/config/env.config.js";
+import { DATABASE_PUBLIC_URL } from "./src/config/env.config.js";
 import { defineConfig } from "prisma/config";
 
 
 export default defineConfig({
 	datasource: {
-		url: DATABASE_URL
+		url: DATABASE_PUBLIC_URL
+
 	}
 });
--- src/config/env.config.ts
+++ src/config/env.config.ts
@@ -2,6 +2,6 @@ import dotenv from 'dotenv';
 dotenv.config()
 
 export const PORT: number = Number.parseInt(process.env.PORT as string) ?? 3000;
-export const DATABASE_URL: string = process.env.DATABASE_URL as string;
+export const DATABASE_PUBLIC_URL: string = process.env.DATABASE_PUBLIC_URL as string;
 export const WORKER_LINK: string = process.env.WORKER_LINK as string;
 export const ORIOKS_LINK: string = process.env.ORIOKS_LINK as string;
