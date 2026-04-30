58bd61e08ec6b0e0e81b58769e6a3c845ecdc05d
Artem Maysky <ijezz@List.ru>
2026-04-30 12:55:21 +0300

    123

--- prisma.config.ts
+++ prisma.config.ts
@@ -1,10 +1,9 @@
-import { DATABASE_PUBLIC_URL } from "./src/config/env.config.js";
+import { DATABASE_URL } from "./src/config/env.config.js";
 import { defineConfig } from "prisma/config";
 
 
 export default defineConfig({
 	datasource: {
-		url: DATABASE_PUBLIC_URL
-
+		url: DATABASE_URL
 	}
 });
